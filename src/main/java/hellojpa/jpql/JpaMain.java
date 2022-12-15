cfe1b7888c425d8004c773e58d59fc03a9fe2d39
gustjdw <johs710@gachon.ac.kr>
2022-12-15 10:05:27 +0900

    조건식(CASE 등등)

--- src/main/java/hellojpa/jpql/JpaMain.java
+++ src/main/java/hellojpa/jpql/JpaMain.java
@@ -17,7 +17,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setAge(10);
             member.setType(MemberType.ADMIN);
             member.setTeam(team);
@@ -26,15 +26,31 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', true from Member m " +
-                    "where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
+
+            String query1 = "select " +
+                    "case when m.age <= 10 then '학생 요금' " +
+                    "when m.age >= 60 then '경로 요금' " +
+                    "else '일반 요금' " +
+                    "end " +
+                    "from Member m";
+            List<String> result1 = em.createQuery(query1, String.class)
                     .getResultList();
-            for (Object[] objects : result) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
+            for (String s : result1) {
+                System.out.println("s = " + s);
+            }
+
+            String query2 = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            List<String> result2 = em.createQuery(query2, String.class)
+                    .getResultList();
+            for (String s : result2) {
+                System.out.println("s = " + s);
+            }
+
+            String query3 = "select nullif(m.username, '관리자') from Member m";
+            List<String> result3 = em.createQuery(query3, String.class)
+                    .getResultList();
+            for (String s : result3) {
+                System.out.println("s = " + s);
             }
 
 
@@ -44,7 +60,9 @@ public class JpaMain {
             e.printStackTrace();
         } finally {
             em.close();
+            System.out.println("em.close()============================================");
         }
         emf.close();
+        System.out.println("emf.close()============================================");
     }
 }
